a234c2848ade6a961290be1e3039c88eff226de4
rennicka <rennicka@sheridancollege.ca>
2024-03-21 23:18:43 -0400

    Week 10 Task3 50% Completed: The buildings spawn in but do not scale up

--- Assets/Week 10/Scripts/Building.cs
+++ Assets/Week 10/Scripts/Building.cs
@@ -12,9 +12,10 @@ public class Building : MonoBehaviour
     public Transform spawnPoint1;
     public Transform spawnPoint2;
     public Transform spawnPoint3;
-    float buildingTimer;
-    public float time;
-    float interpolation;
+    Vector3 minScale;
+    public Vector3 maxScale;
+    public float buildSpeed = 2f;
+    public float time = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +24,9 @@ public class Building : MonoBehaviour
 
     IEnumerator Build()
     {
-        buildingTimer += Time.deltaTime * time;
+        minScale = transform.localScale;
 
-        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        transform.localScale = Vector3.Lerp(maxScale, minScale, time);
         Instantiate(buildingPrefab1, spawnPoint1.position, spawnPoint1.rotation);
         yield return new WaitForSeconds(5);
         Instantiate(buildingPrefab2, spawnPoint2.position, spawnPoint2.rotation);
@@ -36,6 +37,6 @@ public class Building : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        time += 1 * Time.deltaTime;
     }
 }
