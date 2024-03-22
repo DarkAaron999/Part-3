d6d809767baa77c89a78accf5ee752b1c3465cde
rennicka <rennicka@sheridancollege.ca>
2024-03-22 00:46:08 -0400

    Week10 Task4a 50% completed: text displays above the character, the dropdown menu does not select the current character

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class CharacterControl : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI currentSelection;
     public static CharacterControl Instance;
+    public TMP_Dropdown dropdown;
+    public List<GameObject> availableVillagers;
 
     public void Start()
     {
         Instance = this;
+
+        availableVillagers = new List<GameObject>();
     }
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
@@ -24,11 +30,17 @@ public class CharacterControl : MonoBehaviour
         Instance.currentSelection.text = villager.ToString();
     }
 
+    public void DropdownHasChangedValuse(int value)
+    {
+        Debug.Log(value + "character been selected");
+        //SelectedVillager = availableVillagers[value];
+    }
+
     //private void Update()
     //{
-        //if(SelectedVillager != null)
-        //{
-            //currentSelection.text = SelectedVillager.GetType().ToString();
-        //}
+    //if(SelectedVillager != null)
+    //{
+    //currentSelection.text = SelectedVillager.GetType().ToString();
+    //}
     //}
 }
